c9ad5d9c3bc4c28d9f96ee4dc280edbdb0bcaa67
bokovushka <andriibokovyi@gmail.com>
2025-09-16 19:39:07 +0300

    update

--- src/js/modules/js.js
+++ src/js/modules/js.js
@@ -1,10 +1,18 @@
 function setHeroHeight() {
-	const h = window.innerHeight;
+	let h = window.innerHeight;
+
+	// якщо екран вузький і зараз landscape → збільшити у 2 рази
+	if (window.innerWidth < 768 && window.matchMedia('(orientation: landscape)').matches) {
+		h = h * 2;
+	}
+
 	document.documentElement.style.setProperty('--hero-height', `${h}px`);
 }
 
+// одразу при завантаженні
 setHeroHeight();
 
+// при зміні орієнтації
 window.addEventListener('orientationchange', () => {
 	setHeroHeight();
 	window.addEventListener('resize', setHeroHeight, { once: true });
